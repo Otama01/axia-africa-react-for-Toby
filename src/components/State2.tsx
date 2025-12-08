1ba693c033cd098b8a45a2707e4342cfe5bc3860
wondersmasher <wonderfulakinniyi@gmail.com>
2025-12-08 19:14:13 +0100

    chore: instructions

--- src/components/State2.tsx
+++ src/components/State2.tsx
@@ -1,4 +1,9 @@
 const State2 = () => {
+  // Create a state called counter and update the value pending the button clicked...
+  // (when add is clicked add 1, when subtract is clicked, subtract 1)
+
+  // Additional Point => Ensure that your counter does not go below 0
+
   return (
     <div style={{ display: "flex", gap: 1, flexDirection: "column" }}>
       <p>Count: </p>
