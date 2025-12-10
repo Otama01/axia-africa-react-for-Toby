1f994dfc0d4ea5998ed4bcb6002a32e6c83f83b9
wondersmasher <wonderfulakinniyi@gmail.com>
2025-12-10 19:02:50 +0100

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
 import "./App.css";
 import { WeatherComponent } from "./components/context/WeatherComponent";
-import { Component1 } from "./components/CustomHook";
-// import State from "./components/State";
-// import Effect from "./components/Effect";
-// import Card from "./components/Card";
-// import State2 from "./components/State2";
 
 function App() {
-  return (
-    <WeatherComponent />
-    // <Effect />
-    // <Component1 />
-    // <div>
-    //   <Card
-    //     content="This is axia africa"
-    //     course="mobile development"
-    //     id={1}
-    //     isActive={true}
-    //     name="Akinniyi Ezekiel Wonderful"
-    //   />
-    //   <Card
-    //     content="This is axia africa 2"
-    //     course="mobile development"
-    //     id={2}
-    //     isActive={true}
-    //     name="Akinniyi Ezekiel"
-    //   />
-    //   <Card
-    //     content="This is axia africa 3"
-    //     course="mobile development"
-    //     id={3}
-    //     isActive={true}
-    //     name="Akinniyi Wonderful"
-    //   />
-    // </div>
-  );
+  return <WeatherComponent />;
 }
 
 export default App;
