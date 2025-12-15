fc64729a900ef79f1bf707d312d1cf2d127727bc
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2025-12-15 20:04:05 +0100

    feat: add decrement functionality to counter store and update Zustand component

--- src/components/zustand/Zustand.tsx
+++ src/components/zustand/Zustand.tsx
@@ -6,10 +6,16 @@ const Zustand = ({}: Props) => {
   const count2 = useCounterStore((state) => state.count2);
   const incrementCount1 = useCounterStore((state) => state.incrementCount1);
   const incrementCount2 = useCounterStore((state) => state.incrementCount2);
+  const decrementCount1 = useCounterStore((state) => state.decrementCount1);
+  const decrementCount2 = useCounterStore((state) => state.decrementCount2);
   return (
     <div>
-      <button onClick={incrementCount1}>Increment the count</button>
-      <button onClick={incrementCount2}>Increment count2</button>
+      <div style={{ display: "flex", gap: 10 }}>
+        <button onClick={incrementCount1}>Increment the count</button>
+        <button onClick={incrementCount2}>Increment count2</button>
+        <button onClick={decrementCount1}>Decrement the count</button>
+        <button onClick={decrementCount2}>Decrement count2</button>
+      </div>
       <p>Count: {count1}</p>
       <p>count2: {count2}</p>
     </div>
--- src/store/useCounter.ts
+++ src/store/useCounter.ts
@@ -5,9 +5,11 @@ type CounterStoreType = {
   count2: number;
   incrementCount1: () => void;
   incrementCount2: () => void;
+  decrementCount1: () => void;
+  decrementCount2: () => void;
 };
 
-export const useCounterStore = create<CounterStoreType>((set) => ({
+export const useCounterStore = create<CounterStoreType>((set, get) => ({
   count1: 0,
   count2: 10,
   incrementCount1: () => {
@@ -15,5 +17,14 @@ export const useCounterStore = create<CounterStoreType>((set) => ({
   },
   incrementCount2: () => {
     set((state) => ({ count2: state.count2 + 5 }));
+  },
+  decrementCount1: () => {
+    set((state) => ({ count1: state.count1 - 1 }));
+  },
+  decrementCount2: () => {
+    const count2 = get().count2;
+    if (count2 <= 5) return;
+
+    set((state) => ({ count2: state.count2 - 5 }));
   },
 }));
